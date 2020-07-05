176cf5d1c8c2e720af61413d054d95abc0165a61
Evgeny <jen31111@gmail.com>
2020-07-05 17:32:51 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
@@ -8,10 +9,30 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((resolve, reject) => {
+    subscribers.push(resolve);
+
+    ctx.res.on('close', () => {
+      subscribers.splice(subscribers.indexOf(resolve), 1);
+    });
+  });
+
+
+  message = await promise;
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  subscribers.forEach((resolve) => {
+    resolve(message);
+  });
+  subscribers = [];
+  ctx.response.status = 200;
 });
 
 app.use(router.routes());

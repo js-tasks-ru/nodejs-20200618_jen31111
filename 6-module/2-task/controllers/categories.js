9a59154d681a81a7aa662633acacf63b07710bd9
Evgeny <jen31111@gmail.com>
2020-07-08 21:14:07 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,19 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const cat = await Category.find({});
+  const categories = [];
+  cat.forEach((item, index) => {
+    categories.push({
+      id: item._id,
+      title: item.title,
+      subcategories: item.subcategories.map((subcategory) => {
+        return {
+          id: subcategory._id,
+          title: subcategory.title,
+        };
+      }),
+    });
+  });
+  ctx.body = {categories};
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,81 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subCat = ctx.request.query.subcategory;
+
+  if (!!subCat) {
+    const productsBySubCat = await Product.find({subcategory: subCat});
+    ctx.body = {
+      products: productsBySubCat.map((item) => {
+        return {
+          id: item._id,
+          title: item.title,
+          images: item.images.map((image) => {
+            return `${image}`;
+          }),
+          category: item.category,
+          subcategory: item.subcategory,
+          price: item.price,
+          description: item.description,
+        };
+      }),
+
+    };
+  } else {
+    const products = await Product.find({});
+    ctx.body = {
+      products: products.map((item) => {
+        return {
+          id: item._id,
+          title: item.title,
+          images: item.images.map((image) => {
+            return `${image}`;
+          }),
+          category: item.category,
+          subcategory: item.subcategory,
+          price: item.price,
+          description: item.description,
+        };
+      }),
+
+    };
+  }
+  // console.log(Products);
+  if (!ctx.body.products) {
+    ctx.throw(404);
+  }
+  next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!mongoose.isValidObjectId(ctx.params.id)) {
+    ctx.throw(400);
+  } else {
+    const id = new mongoose.Types.ObjectId(ctx.params.id);
+    const product = await Product.findOne(id);
+
+    if (!product) {
+      ctx.throw(404);
+    }
+
+    ctx.body = {
+      product: {
+        id: product._id,
+        title: product.title,
+        images: product.images,
+        category: product.category,
+        subcategory: product.subcategory,
+        price: product.price,
+        description: product.description,
+      },
+    };
+    // console.log(ctx.body.product);
+  }
+  next();
 };
 
